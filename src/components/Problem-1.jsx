0f2e1189a0a2862f5bac5063c4cb50c894355e7a
devanondo <dev.abormon28@gmail.com>
2023-12-19 21:57:12 +0600

    [DONE] PROLEM-1

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,10 +1,78 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 
 const Problem1 = () => {
   const [show, setShow] = useState("all");
+  const [formData, setFormData] = useState({
+    name: "",
+    status: "",
+  });
+  const [tasks, setTasks] = useState([]);
+
+  const [sortedTasks, setSortedTasks] = useState(tasks);
+
+  const handleFormData = (e) => {
+    const value =
+      e.target.name === "status"
+        ? e.target.value.toLowerCase()
+        : e.target.value;
+
+    setFormData({ ...formData, [e.target.name]: value });
+  };
+
+  useEffect(() => {
+    const sors = sortTask();
+    setSortedTasks(sors);
+    setTasks(sors);
+  }, [tasks]);
+
+  const handleAddTask = () => {
+    setTasks([...tasks, formData]);
+    setSortedTasks([...sortedTasks, formData]);
+    setFormData({
+      name: "",
+      status: "",
+    });
+  };
+
+  const filterTask = (val) => {
+    switch (val) {
+      case "active":
+        return tasks.filter((task) => task.status.toLowerCase() === "active");
+
+      case "completed":
+        return tasks.filter(
+          (task) => task.status.toLowerCase() === "completed"
+        );
+
+      default:
+        return tasks;
+    }
+  };
 
   const handleClick = (val) => {
     setShow(val);
+    const data = filterTask(val);
+    setSortedTasks(data);
+  };
+
+  const sortTask = () => {
+    const tsk = tasks.sort((a, b) => {
+      if (a.status === b.status) {
+        return 0;
+      }
+
+      if (
+        a.status === "active" ||
+        (a.status === "completed" && b.status !== "active")
+      ) {
+        return -1;
+      }
+
+      return 1;
+    });
+
+    return tsk;
   };
 
   return (
@@ -12,15 +80,33 @@ const Problem1 = () => {
       <div className="row justify-content-center mt-5">
         <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
         <div className="col-6 ">
-          <form className="row gy-2 gx-3 align-items-center mb-4">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleAddTask();
+            }}
+            className="row gy-2 gx-3 align-items-center mb-4"
+          >
             <div className="col-auto">
-              <input type="text" className="form-control" placeholder="Name" />
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleFormData}
+                className="form-control"
+                placeholder="Name"
+                required
+              />
             </div>
             <div className="col-auto">
               <input
                 type="text"
+                name="status"
+                value={formData.status}
+                onChange={handleFormData}
                 className="form-control"
                 placeholder="Status"
+                required
               />
             </div>
             <div className="col-auto">
@@ -68,7 +154,14 @@ const Problem1 = () => {
                 <th scope="col">Status</th>
               </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+              {sortedTasks.map((task, index) => (
+                <tr key={index}>
+                  <td>{task.name}</td>
+                  <td>{task.status}</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
